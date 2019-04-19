e13dd30125d504529c3ab82b2c711841e238dac6
MaschenkoVlad <43501484+MaschenkoVlad@users.noreply.github.com>
2019-04-19 16:21:29 +0300

    Upload new file

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import Form from './loginForm';
 
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { createMuiTheme, MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+import styles from '../style/css/style'
+
 class App extends React.Component {
-  
     submit = ( values ) => {
         console.log( values );
         console.log(JSON.stringify(values));
     }
     render() {
         return (
-        <div>
-            <h3>Authentication</h3>
-            <Form onSubmit={this.submit}/>
-        </div>
-        
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={ styles.paperContainer } >
+                
+                <Grid item xs={5}
+                      style={ styles.formContainer} >
+                    <Typography variant="h4" gutterBottom style={ styles.textHeadline } 
+                    >Don't have an account?</Typography>
+                    <Form onSubmit={this.submit}/>
+                </Grid>    
+            </Grid>
         )
     }
 }
-
 export default App;
+
--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -3,68 +3,80 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux'
 import { signInFunc } from '../actions/actions'
 
-
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { createMuiTheme, MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+import styles from '../style/css/style'
 
 const validate = ( values ) => {
-
     const errors = {}
-    
     if (!values.login) {
       errors.login = 'Required'
     } 
-    
     if (!values.email) {
       errors.email = 'Required'
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
       errors.email = 'Invalid email address'
     }
-
     return errors
   }
 
-const Input = ( props ) => {
-
+const input = ( props ) => {
     const { meta } = props;
     return (
         <Fragment>
-
-            <input {...props.input} type={props.type} placeholder={props.placeholder} value={props.value} />
-
+            <TextField {...props.input} 
+                  type={props.type} 
+                  placeholder={props.placeholder} 
+                  value={props.value} 
+                  style={styles.textField} 
+                  InputProps={{
+                    style: {
+                        color: "#f5f5f5",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 16
+                    }
+                  }} />
             {/* сначала проверяет, где есть какие-либо данные, хранящиеся внутри объекта error. 
             Если это так, он проверяет, взаимодействовал ли пользователь с полем ввода. 
             Если это так, то приложение вернет данные, хранящиеся внутри объекта error. */}
-            {meta.error && meta.touched &&
-            <div>{meta.error}</div>}
-
+            {meta.error && meta.touched && <div style={styles.errorField}>{meta.error}</div>}
         </Fragment>
     )
 }
 
-
 let Form = ( props ) => {
-
-  const { handleSubmit, pristine, submitting, signInFunc} = props;
+  const { handleSubmit, pristine, submitting, signInFunc  } = props;
   return (
-    <form onSubmit={ (e)=>{ signInFunc(e.target.elements.login.value, 
-                                       e.target.elements.email.value );
-                            handleSubmit(); 
-                            e.preventDefault(); }}>
-
-        {/* Name prop действует как идентификатор поля */}
-        {/* component prop ссылается на элемент html */}
-      <div>
-        <Field name="login" component={Input} label="Login" type="text" placeholder="Login" /> 
-      </div>
-
-      <div>
-        <Field name="email" component={Input} label="Email" type="email" placeholder="Email" />
-      </div>
-
-      <div>
-        <button type="submit" disabled={pristine || submitting} >Submit</button>
-      </div>
-
-    </form>
+      <form onSubmit={ (e)=>{ signInFunc(e.target.elements.login.value, 
+                                        e.target.elements.email.value );
+                              handleSubmit(); 
+                              e.preventDefault(); 
+                    }} 
+            autoComplete="off" >
+          {/* Name prop действует как идентификатор поля */}
+          {/* component prop ссылается на элемент html */}
+        <div style={styles.inputBox}>
+          <Field name="login" 
+                 component={input} 
+                 label="Login" 
+                 type="text"
+                 placeholder="Login" /> 
+        </div>
+        <div style={styles.inputBox}>
+          <Field name="email" 
+                 component={input} 
+                 label="Email" 
+                 type="email" 
+                 placeholder="Email"  />
+        </div>
+        <div>
+          <Button type="submit" 
+                  disabled={pristine || submitting}  
+                  fullWidth 
+                  variant="contained" >SIGN UP</Button>
+        </div>
+      </form>
   )
 }
 
